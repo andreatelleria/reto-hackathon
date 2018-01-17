d6f958cef248371cc2b1b46b74f813eeea5d5512
PaoSil <pshuaman@outlook.com>
2018-01-17 16:14:22 -0500

    adding main.css, index.html, app.js y views

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
-
-  
+  setTimeout(function () {
+	window.location.href = 'views/home.html';
+}, 5000);
+$("#typed").typed({
+	strings: ["Niños Felices, Padres Felices"],
+	typeSpeed: 30,
+	backDelay: 500,
+	loop: false,
+	loopCount: false,
+	callback: function () { foo(); }
+});
+function foo() { console.log("Callback"); }
 });
