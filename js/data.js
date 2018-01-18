428430fd027d63b0a80bebebfa68ea4f503d1322
betsy18 <betsy_dessire@hotmail.com>
2018-01-18 18:33:26 -0500

    Adding data.js

---
+++ js/data.js
@@ -0,0 +1,163 @@
+var peliculas = {
+  'adventure' : [ 
+    {title: 'Finding Nemo',
+    genre: 'adventure',
+    director: 'Andrew Stanton, Lee Unkrich(co-director)',
+    writer: 'Andrew Stanton (original story by), Andrew Stanton (screenplay by), Bob Peterson (screenplay by), David Reynolds (screenplay by)',
+    actors: 'Albert Brooks, Ellen DeGeneres, Alexander Gould, Willem Dafoe'
+    },
+    {title: 'The Incredibles',
+    genre: 'adventure',
+    director: 'Brad Bird',
+    writer: 'Brad Bird',
+    actors: 'Craig T. Nelson, Holly Hunter, Samuel L. Jackson, Jason Lee'
+    },
+    {title: 'Up',
+    genre: 'adventure',
+    director: 'Pete Docter, Bob Peterson(co-director)',
+    writer: 'Pete Docter (story by), Bob Peterson (story by), Tom McCarthy (story by), Bob Peterson (screenplay by), Pete Docter (screenplay by)',
+    actors: 'Edward Asner, Christopher Plummer, Jordan Nagai, Bob Peterson'
+    },
+    {title: 'Frozen',
+    genre: 'adventure',
+    director: 'Chris Buck, Jennifer Lee',
+    writer: 'Jennifer Lee (screenplay by), Hans Christian Andersen (story inspired by \"The Snow Queen\" by), Chris Buck (story by), Jennifer Lee (story by), Shane Morris (story by)',
+    actors: 'Kristen Bell, Idina Menzel, Jonathan Groff, Josh Gad'
+    },
+    {title: 'Wreck-It Ralph',
+    genre: 'adventure',
+    director: 'Rich Moore',
+    writer: 'Rich Moore (story by), Phil Johnston (story by), Jim Reardon (story by), Phil Johnston (screenplay by), Jennifer Lee (screenplay by), John C. Reilly (additional story material by), Sam Levine (additional story material by), Jared Stern (additional story material by)',
+    actors: 'John C. Reilly, Sarah Silverman, Jack McBrayer, Jane Lynch'
+    },
+  ],
+  'ciencia' : [
+    {title: 'WALL·E',
+    genre: 'science-fiction',
+    director: 'Andrew Stanton',
+    writer: 'Andrew Stanton (original story by), Pete Docter (original story by), Andrew Stanton (screenplay by), Jim Reardon (screenplay by)',
+    actors: 'Ben Burtt, Elissa Knight, Jeff Garlin, Fred Willard'
+    },
+    {title: '',
+    genre: 'science-fiction',
+    director: '',
+    writer: '',
+    actors: ''
+    },
+    {title: '',
+    genre: 'science-fiction',
+    director: '',
+    writer: '',
+    actors: ''
+    },
+    {title: '',
+    genre: 'science-fiction',
+    director: '',
+    writer: '',
+    actors: ''
+    },
+    {title: '',
+    genre: 'science-fiction',
+    director: '',
+    writer: '',
+    actors: ''
+    },
+  ],
+  'comedia': [
+    {title: 'The Boss Baby',
+    genre: 'comedy',
+    director: 'Tom McGrath',
+    writer: 'Michael McCullers, Marla Frazee (based on the book by)',
+    actors: 'Alec Baldwin, Steve Buscemi, Jimmy Kimmel, Lisa Kudrow'
+    },
+    {title: '',
+    genre: 'comedy',
+    director: '',
+    writer: '',
+    actors: ''
+    },
+    {title: '',
+    genre: 'comedy',
+    director: '',
+    writer: '',
+    actors: ''
+    },
+    {title: '',
+    genre: 'comedy',
+    director: '',
+    writer: '',
+    actors: ''
+    },
+    {title: '',
+    genre: 'comedy',
+    director: '',
+    writer: '',
+    actors: ''
+    },
+  ],
+  'fantasia':[
+    {title: '',
+    genre: '',
+    director: '',
+    writer: '',
+    actors: ''
+    },
+    {title: '',
+    genre: '',
+    director: '',
+    writer: '',
+    actors: ''
+    },
+    {title: '',
+    genre: '',
+    director: '',
+    writer: '',
+    actors: ''
+    },
+    {title: '',
+    genre: '',
+    director: '',
+    writer: '',
+    actors: ''
+    },
+    {title: '',
+    genre: '',
+    director: '',
+    writer: '',
+    actors: ''
+    },
+  ],
+  'musical': [
+    {title: '',
+    genre: '',
+    director: '',
+    writer: '',
+    actors: ''
+    },
+    {title: '',
+    genre: '',
+    director: '',
+    writer: '',
+    actors: ''
+    },
+    {title: '',
+    genre: '',
+    director: '',
+    writer: '',
+    actors: ''
+    },
+    {title: '',
+    genre: '',
+    director: '',
+    writer: '',
+    actors: ''
+    },
+    {title: '',
+    genre: '',
+    director: '',
+    writer: '',
+    actors: ''
+    },
+  ]
+
+}
