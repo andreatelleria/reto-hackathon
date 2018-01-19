4c1f75071ad7a187910180dd6483a8ac014e3938
stephaniewv <stephaniewv@gmail.com>
2018-01-19 08:55:15 -0500

    upload firebase

---
+++ vendors/js/firebaseapp.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  // Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAlknV1ienCHDm_twqLWUw4iBzxtBZgX4M",
+    authDomain: "kidmovies-7dda5.firebaseapp.com",
+    databaseURL: "https://kidmovies-7dda5.firebaseio.com",
+    projectId: "kidmovies-7dda5",
+    storageBucket: "kidmovies-7dda5.appspot.com",
+    messagingSenderId: "689512485282"
+  };
+  firebase.initializeApp(config);
+});
